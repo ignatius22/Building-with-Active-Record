dcfeb2ae0e834ee5fab13d4359478a31f5f53bcd
Ignatius22 <igee54077@gmail.com>
2020-07-29 08:00:53 +0100

    fix lint errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :title, presence: true ,length: {minimum: 6, maximum: 100}
-  validates :text, presence: true, length: {minimum: 10, maximum: 5000}
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+  validates :text, presence: true, length: { minimum: 10, maximum: 5000 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments
-  
+
   validates :username, uniqueness: true, length: { within: 4..12 }, presence: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i	
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true,
-					  uniqueness: { case_sensitive: false },
-					  length:{ maximum: 105 },
-					  format: {with: VALID_EMAIL_REGEX}
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { maximum: 6 }, presence: true
 end
