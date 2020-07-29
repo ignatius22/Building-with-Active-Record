69ac3122cc0d8b2d36cf718633927871c040a44c
Ignatius22 <igee54077@gmail.com>
2020-07-29 05:22:29 +0100

    fix linter issue and optional requirement implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :comments
   
   validates :username, uniqueness: true, length: { within: 4..12 }, presence: true
-  validates :email, uniqueness: true, presence: true
-  validates :password, length: { within: 4..12 }, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i	
+  validates :email, presence: true,
+					  uniqueness: { case_sensitive: false },
+					  length:{ maximum: 105 },
+					  format: {with: VALID_EMAIL_REGEX}
+  validates :password, length: { maximum: 6 }, presence: true
 end
